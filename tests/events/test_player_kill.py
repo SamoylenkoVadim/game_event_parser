ba59a34406aead4c11f75fd0875151a3efdabe2c
Vadim Samoilenko <vsam26@yandex.ru>
2023-06-25 17:21:41 +0200

    tests type for player_id fixes. int->str

--- tests/events/test_player_kill.py
+++ tests/events/test_player_kill.py
@@ -3,9 +3,9 @@ from main import Event
 
 def test_apply_player_kill_event(game):
     event_payload = {
-        "killerID": 1,
-        "victimID": 6,
-        "assistants": [2],
+        "killerID": "player1",
+        "victimID": "player6",
+        "assistants": ["player2"],
         "goldGranted": 100,
         "assistGold": 50
     }
@@ -13,9 +13,9 @@ def test_apply_player_kill_event(game):
 
     event.apply(game)
 
-    killer = game.get_player(1)
-    victim = game.get_player(6)
-    assistant = game.get_player(2)
+    killer = game.get_player("player1")
+    victim = game.get_player("player6")
+    assistant = game.get_player("player2")
 
     assert killer.player_kills == 1
     assert killer.gold == 200
@@ -24,11 +24,11 @@ def test_apply_player_kill_event(game):
     assert assistant.gold == 250
 
 
-def test_apply_player_kill_event_invalid_killer(game):
+def test_apply_player_kill_event_none_killer(game):
     event_payload = {
-        "killerID": 100,
-        "victimID": 2,
-        "assistants": [6],
+        "killerID": None,
+        "victimID": "player2",
+        "assistants": ["player6"],
         "goldGranted": 100,
         "assistGold": 50
     }
@@ -36,9 +36,29 @@ def test_apply_player_kill_event_invalid_killer(game):
 
     event.apply(game)
 
-    killer = game.get_player(100)
-    victim = game.get_player(2)
-    assistant = game.get_player(6)
+    victim = game.get_player("player2")
+    assistant = game.get_player("player6")
+
+    assert victim.alive is False
+    assert assistant.assists == 1
+    assert assistant.gold == 150
+
+
+def test_apply_player_kill_event_invalid_killer(game):
+    event_payload = {
+        "killerID": "100",
+        "victimID": "player2",
+        "assistants": ["player6"],
+        "goldGranted": 100,
+        "assistGold": 50
+    }
+    event = Event(event_type='PLAYER_KILL', payload=event_payload)
+
+    event.apply(game)
+
+    killer = game.get_player("100")
+    victim = game.get_player("player2")
+    assistant = game.get_player("player6")
 
     assert killer is None
     assert victim.alive is False
@@ -48,8 +68,8 @@ def test_apply_player_kill_event_invalid_killer(game):
 
 def test_apply_player_kill_event_no_killer(game):
     event_payload = {
-        "victimID": 2,
-        "assistants": [6],
+        "victimID": "player2",
+        "assistants": ["player6"],
         "goldGranted": 100,
         "assistGold": 50
     }
@@ -57,19 +77,19 @@ def test_apply_player_kill_event_no_killer(game):
 
     event.apply(game)
 
-    victim = game.get_player(2)
-    assistant = game.get_player(6)
+    victim = game.get_player("player2")
+    assistant = game.get_player("player6")
 
     assert victim.alive is False
     assert assistant.assists == 1
     assert assistant.gold == 150
 
 
-def test_apply_player_kill_event_invalid_killer_2(game):
+def test_apply_player_kill_event_invalid_killer_type(game):
     event_payload = {
-        "killerID": "1",
-        "victimID": 2,
-        "assistants": [6],
+        "killerID": 1,
+        "victimID": "player2",
+        "assistants": ["player6"],
         "goldGranted": 100,
         "assistGold": 50
     }
@@ -77,21 +97,39 @@ def test_apply_player_kill_event_invalid_killer_2(game):
 
     event.apply(game)
 
-    killer = game.get_player(1)
-    victim = game.get_player(2)
-    assistant = game.get_player(6)
+    victim = game.get_player("player2")
+    assistant = game.get_player("player6")
 
-    assert killer.player_kills == 0
     assert victim.alive is False
     assert assistant.assists == 1
     assert assistant.gold == 150
+
+
+def test_apply_player_kill_event_none_victim(game):
+    event_payload = {
+        "killerID": "player1",
+        "victimID": None,
+        "assistants": ["player2"],
+        "goldGranted": 100,
+        "assistGold": 50
+    }
+    event = Event(event_type='PLAYER_KILL', payload=event_payload)
+
+    event.apply(game)
+
+    killer = game.get_player("player1")
+    assistant = game.get_player("player2")
+
+    assert killer.player_kills == 1
+    assert assistant.assists == 1
+    assert assistant.gold == 250
 
 
 def test_apply_player_kill_event_invalid_victim(game):
     event_payload = {
-        "killerID": 1,
-        "victimID": 100,
-        "assistants": [2],
+        "killerID": "player1",
+        "victimID": "100",
+        "assistants": ["player2"],
         "goldGranted": 100,
         "assistGold": 50
     }
@@ -99,9 +137,48 @@ def test_apply_player_kill_event_invalid_victim(game):
 
     event.apply(game)
 
-    killer = game.get_player(1)
+    killer = game.get_player("player1")
+    assistant = game.get_player("player2")
+
+    assert killer.player_kills == 1
+    assert assistant.assists == 1
+    assert assistant.gold == 250
+
+
+def test_apply_player_kill_event_no_victim(game):
+    event_payload = {
+        "killerID": "player1",
+        "assistants": ["player2"],
+        "goldGranted": 100,
+        "assistGold": 50
+    }
+    event = Event(event_type='PLAYER_KILL', payload=event_payload)
+
+    event.apply(game)
+
+    killer = game.get_player("player1")
+    assistant = game.get_player("player2")
+
+    assert killer.player_kills == 1
+    assert assistant.assists == 1
+    assert assistant.gold == 250
+
+
+def test_apply_player_kill_event_invalid_victim_type(game):
+    event_payload = {
+        "killerID": "player1",
+        "victimID": 100,
+        "assistants": ["player2"],
+        "goldGranted": 100,
+        "assistGold": 50
+    }
+    event = Event(event_type='PLAYER_KILL', payload=event_payload)
+
+    event.apply(game)
+
+    killer = game.get_player("player1")
     victim = game.get_player(100)
-    assistant = game.get_player(2)
+    assistant = game.get_player("player2")
 
     assert killer.player_kills == 1
     assert victim is None
@@ -109,10 +186,11 @@ def test_apply_player_kill_event_invalid_victim(game):
     assert assistant.gold == 250
 
 
-def test_apply_player_kill_event_no_victim(game):
+def test_apply_player_kill_event_invalid_assistant_in_list(game):
     event_payload = {
-        "killerID": 1,
-        "assistants": [2],
+        "killerID": "player1",
+        "victimID": "player6",
+        "assistants": ["a", "player2"],
         "goldGranted": 100,
         "assistGold": 50
     }
@@ -120,52 +198,10 @@ def test_apply_player_kill_event_no_victim(game):
 
     event.apply(game)
 
-    killer = game.get_player(1)
-    assistant = game.get_player(2)
-
-    assert killer.player_kills == 1
-    assert assistant.assists == 1
-    assert assistant.gold == 250
-
-
-def test_apply_player_kill_event_invalid_victim_2(game):
-    event_payload = {
-        "killerID": 1,
-        "victimID": "6",
-        "assistants": [2],
-        "goldGranted": 100,
-        "assistGold": 50
-    }
-    event = Event(event_type='PLAYER_KILL', payload=event_payload)
-
-    event.apply(game)
-
-    killer = game.get_player(1)
-    victim = game.get_player(6)
-    assistant = game.get_player(2)
-
-    assert killer.player_kills == 1
-    assert victim.alive is True
-    assert assistant.assists == 1
-    assert assistant.gold == 250
-
-
-def test_apply_player_kill_event_invalid_assistants(game):
-    event_payload = {
-        "killerID": 1,
-        "victimID": 6,
-        "assistants": ["a", 2],
-        "goldGranted": 100,
-        "assistGold": 50
-    }
-    event = Event(event_type='PLAYER_KILL', payload=event_payload)
-
-    event.apply(game)
-
-    killer = game.get_player(1)
-    victim = game.get_player(6)
+    killer = game.get_player("player1")
+    victim = game.get_player("player6")
     assistant1 = game.get_player("a")
-    assistant2 = game.get_player(2)
+    assistant2 = game.get_player("player2")
 
     assert killer.player_kills == 1
     assert victim.alive is False
@@ -174,11 +210,11 @@ def test_apply_player_kill_event_invalid_assistants(game):
     assert assistant2.gold == 250
 
 
-def test_apply_player_kill_event_invalid_assistants_2(game):
+def test_apply_player_kill_event_invalid_type_assistant_in_list(game):
     event_payload = {
-        "killerID": 1,
-        "victimID": 6,
-        "assistants": ["3", 2],
+        "killerID": "player1",
+        "victimID": "player6",
+        "assistants": [3, "player2"],
         "goldGranted": 100,
         "assistGold": 50
     }
@@ -186,10 +222,10 @@ def test_apply_player_kill_event_invalid_assistants_2(game):
 
     event.apply(game)
 
-    killer = game.get_player(1)
-    victim = game.get_player(6)
+    killer = game.get_player("player1")
+    victim = game.get_player("player6")
     assistant1 = game.get_player("3")
-    assistant2 = game.get_player(2)
+    assistant2 = game.get_player("player2")
 
     assert killer.player_kills == 1
     assert victim.alive is False
@@ -200,8 +236,8 @@ def test_apply_player_kill_event_invalid_assistants_2(game):
 
 def test_apply_player_kill_event_invalid_assistants_type(game):
     event_payload = {
-        "killerID": 1,
-        "victimID": 6,
+        "killerID": "player1",
+        "victimID": "player6",
         "assistants": 2,
         "goldGranted": 100,
         "assistGold": 50
@@ -210,9 +246,9 @@ def test_apply_player_kill_event_invalid_assistants_type(game):
 
     event.apply(game)
 
-    killer = game.get_player(1)
-    victim = game.get_player(6)
-    assistant = game.get_player(2)
+    killer = game.get_player("player1")
+    victim = game.get_player("player6")
+    assistant = game.get_player("player2")
 
     assert killer.player_kills == 1
     assert victim.alive is False
@@ -220,10 +256,11 @@ def test_apply_player_kill_event_invalid_assistants_type(game):
     assert assistant.gold == 200
 
 
-def test_apply_player_kill_event_no_assistants(game):
+def test_apply_player_kill_event_none_assistants(game):
     event_payload = {
-        "killerID": 1,
-        "victimID": 6,
+        "killerID": "player1",
+        "victimID": "player6",
+        "assistants": None,
         "goldGranted": 100,
         "assistGold": 50
     }
@@ -231,8 +268,27 @@ def test_apply_player_kill_event_no_assistants(game):
 
     event.apply(game)
 
-    killer = game.get_player(1)
-    victim = game.get_player(6)
+    killer = game.get_player("player1")
+    victim = game.get_player("player6")
+
+    assert killer.player_kills == 1
+    assert killer.gold == 200
+    assert victim.alive is False
+
+
+def test_apply_player_kill_event_no_assistants(game):
+    event_payload = {
+        "killerID": "player1",
+        "victimID": "player6",
+        "goldGranted": 100,
+        "assistGold": 50
+    }
+    event = Event(event_type='PLAYER_KILL', payload=event_payload)
+
+    event.apply(game)
+
+    killer = game.get_player("player1")
+    victim = game.get_player("player6")
 
     assert killer.player_kills == 1
     assert killer.gold == 200
@@ -241,18 +297,18 @@ def test_apply_player_kill_event_no_assistants(game):
 
 def test_apply_player_kill_event_no_gold_granted(game):
     event_payload = {
-        "killerID": 1,
-        "victimID": 6,
-        "assistants": [2],
+        "killerID": "player1",
+        "victimID": "player6",
+        "assistants": ["player2"],
         "assistGold": 50
     }
     event = Event(event_type='PLAYER_KILL', payload=event_payload)
 
     event.apply(game)
 
-    killer = game.get_player(1)
-    victim = game.get_player(6)
-    assistant = game.get_player(2)
+    killer = game.get_player("player1")
+    victim = game.get_player("player6")
+    assistant = game.get_player("player2")
 
     assert killer.player_kills == 1
     assert killer.gold == 100
@@ -263,9 +319,9 @@ def test_apply_player_kill_event_no_gold_granted(game):
 
 def test_apply_player_kill_event_none_gold_granted(game):
     event_payload = {
-        "killerID": 1,
-        "victimID": 6,
-        "assistants": [2],
+        "killerID": "player1",
+        "victimID": "player6",
+        "assistants": ["player2"],
         "goldGranted": None,
         "assistGold": 50
     }
@@ -273,9 +329,9 @@ def test_apply_player_kill_event_none_gold_granted(game):
 
     event.apply(game)
 
-    killer = game.get_player(1)
-    victim = game.get_player(6)
-    assistant = game.get_player(2)
+    killer = game.get_player("player1")
+    victim = game.get_player("player6")
+    assistant = game.get_player("player2")
 
     assert killer.player_kills == 1
     assert killer.gold == 100
@@ -286,9 +342,9 @@ def test_apply_player_kill_event_none_gold_granted(game):
 
 def test_apply_player_kill_event_invalid_gold_granted(game):
     event_payload = {
-        "killerID": 1,
-        "victimID": 6,
-        "assistants": [2],
+        "killerID": "player1",
+        "victimID": "player6",
+        "assistants": ["player2"],
         "goldGranted": "100",
         "assistGold": 50
     }
@@ -296,9 +352,9 @@ def test_apply_player_kill_event_invalid_gold_granted(game):
 
     event.apply(game)
 
-    killer = game.get_player(1)
-    victim = game.get_player(6)
-    assistant = game.get_player(2)
+    killer = game.get_player("player1")
+    victim = game.get_player("player6")
+    assistant = game.get_player("player2")
 
     assert killer.player_kills == 1
     assert killer.gold == 100
@@ -309,9 +365,9 @@ def test_apply_player_kill_event_invalid_gold_granted(game):
 
 def test_apply_player_kill_event_invalid_gold_granted_2(game):
     event_payload = {
-        "killerID": 1,
-        "victimID": 6,
-        "assistants": [2],
+        "killerID": "player1",
+        "victimID": "player6",
+        "assistants": ["player2"],
         "goldGranted": "a",
         "assistGold": 50
     }
@@ -319,9 +375,9 @@ def test_apply_player_kill_event_invalid_gold_granted_2(game):
 
     event.apply(game)
 
-    killer = game.get_player(1)
-    victim = game.get_player(6)
-    assistant = game.get_player(2)
+    killer = game.get_player("player1")
+    victim = game.get_player("player6")
+    assistant = game.get_player("player2")
 
     assert killer.player_kills == 1
     assert killer.gold == 100
@@ -332,64 +388,18 @@ def test_apply_player_kill_event_invalid_gold_granted_2(game):
 
 def test_apply_player_kill_event_no_assist_gold(game):
     event_payload = {
-        "killerID": 1,
-        "victimID": 6,
-        "assistants": [2],
+        "killerID": "player1",
+        "victimID": "player6",
+        "assistants": ["player2"],
         "goldGranted": 100
     }
     event = Event(event_type='PLAYER_KILL', payload=event_payload)
 
     event.apply(game)
 
-    killer = game.get_player(1)
-    victim = game.get_player(6)
-    assistant = game.get_player(2)
-
-    assert killer.player_kills == 1
-    assert killer.gold == 200
-    assert victim.alive is False
-    assert assistant.assists == 1
-    assert assistant.gold == 200
-
-
-def test_apply_player_kill_event_invalid_assist_gold(game):
-    event_payload = {
-        "killerID": 1,
-        "victimID": 6,
-        "assistants": [2],
-        "goldGranted": 100,
-        "assistGold": "50"
-    }
-    event = Event(event_type='PLAYER_KILL', payload=event_payload)
-
-    event.apply(game)
-
-    killer = game.get_player(1)
-    victim = game.get_player(6)
-    assistant = game.get_player(2)
-
-    assert killer.player_kills == 1
-    assert killer.gold == 200
-    assert victim.alive is False
-    assert assistant.assists == 1
-    assert assistant.gold == 200
-
-
-def test_apply_player_kill_event_invalid_assist_gold_2(game):
-    event_payload = {
-        "killerID": 1,
-        "victimID": 6,
-        "assistants": [2],
-        "goldGranted": 100,
-        "assistGold": "a"
-    }
-    event = Event(event_type='PLAYER_KILL', payload=event_payload)
-
-    event.apply(game)
-
-    killer = game.get_player(1)
-    victim = game.get_player(6)
-    assistant = game.get_player(2)
+    killer = game.get_player("player1")
+    victim = game.get_player("player6")
+    assistant = game.get_player("player2")
 
     assert killer.player_kills == 1
     assert killer.gold == 200
@@ -400,9 +410,9 @@ def test_apply_player_kill_event_invalid_assist_gold_2(game):
 
 def test_apply_player_kill_event_none_assist_gold(game):
     event_payload = {
-        "killerID": 1,
-        "victimID": 6,
-        "assistants": [2],
+        "killerID": "player1",
+        "victimID": "player6",
+        "assistants": ["player2"],
         "goldGranted": 100,
         "assistGold": None
     }
@@ -410,12 +420,60 @@ def test_apply_player_kill_event_none_assist_gold(game):
 
     event.apply(game)
 
-    killer = game.get_player(1)
-    victim = game.get_player(6)
-    assistant = game.get_player(2)
+    killer = game.get_player("player1")
+    victim = game.get_player("player6")
+    assistant = game.get_player("player2")
 
     assert killer.player_kills == 1
     assert killer.gold == 200
     assert victim.alive is False
     assert assistant.assists == 1
     assert assistant.gold == 200
+
+
+def test_apply_player_kill_event_invalid_assist_gold(game):
+    event_payload = {
+        "killerID": "player1",
+        "victimID": "player6",
+        "assistants": ["player2"],
+        "goldGranted": 100,
+        "assistGold": "50"
+    }
+    event = Event(event_type='PLAYER_KILL', payload=event_payload)
+
+    event.apply(game)
+
+    killer = game.get_player("player1")
+    victim = game.get_player("player6")
+    assistant = game.get_player("player2")
+
+    assert killer.player_kills == 1
+    assert killer.gold == 200
+    assert victim.alive is False
+    assert assistant.assists == 1
+    assert assistant.gold == 200
+
+
+def test_apply_player_kill_event_invalid_assist_gold_2(game):
+    event_payload = {
+        "killerID": "player1",
+        "victimID": "player6",
+        "assistants": ["player2"],
+        "goldGranted": 100,
+        "assistGold": "a"
+    }
+    event = Event(event_type='PLAYER_KILL', payload=event_payload)
+
+    event.apply(game)
+
+    killer = game.get_player("player1")
+    victim = game.get_player("player6")
+    assistant = game.get_player("player2")
+
+    assert killer.player_kills == 1
+    assert killer.gold == 200
+    assert victim.alive is False
+    assert assistant.assists == 1
+    assert assistant.gold == 200
+
+
